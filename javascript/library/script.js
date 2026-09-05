702171b588eab95aece4fb325a3a5137fa86aa11
Noorullah <itxnoor148@gmail.com>
2026-09-05 22:52:40 +0500

    Create the Book and myLibrary

--- javascript/library/script.js
+++ javascript/library/script.js
@@ -2,5 +2,14 @@
 // LIBRARY DATA
 // ========================================
 
-const a='hello'
-console.log(a)
+const myLibrary=[]
+
+// Book constructor
+
+function Book(title,auther,pages,read){
+    this.title=title
+    this.auther=auther
+    this.pages=pages
+    this.read=read
+    this.id=crypto.randomUUID()
+}
