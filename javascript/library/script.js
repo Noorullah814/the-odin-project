9ecd369dd9ecb7c954baf93984bf49485ff7d479
Noorullah <itxnoor148@gmail.com>
2026-09-13 22:09:07 +0500

    Add persist theme preference

--- javascript/library/script.js
+++ javascript/library/script.js
@@ -246,4 +246,14 @@ bookDialog.addEventListener("click", (event) => {
 
 themeToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme");
+
+    const isDark = document.body.classList.contains("dark-theme");
+
+    localStorage.setItem("theme", isDark ? "dark" : "light");
 });
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-theme");
+}
