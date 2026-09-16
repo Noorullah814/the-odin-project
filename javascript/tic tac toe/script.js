b30584f915bcbce37f164b5022529cb62f6cffc6
Noorullah <itxnoor148@gmail.com>
2026-09-16 20:43:00 +0500

    Implement Game Controller and game flow logic

--- javascript/tic tac toe/script.js
+++ javascript/tic tac toe/script.js
@@ -5,10 +5,11 @@ const Gameboard = (function () {
     const placeMark = (mark, index) => {
         if (board[index] != "") {
             console.log("move failed")
-            return
+            return false
         }
 
         board[index] = mark
+        return true
 
     }
 
@@ -17,10 +18,9 @@ const Gameboard = (function () {
         return board
     }
 
-    const resetBoard=()=>{
-        for(let i=0;i<9;i++)
-        {
-            board[i]=""
+    const resetBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            board[i] = ""
         }
     }
 
@@ -32,7 +32,88 @@ const Gameboard = (function () {
 
 })()
 
-function Player(name,mark)
-{
-    return {name,mark}
+function Player(name, mark) {
+    return { name, mark }
 }
+
+const GameController = (function () {
+
+    const playerOne = Player("Noor", "X")
+    const playerTwo = Player("Yar", "O")
+
+    let activePlayer = playerOne
+
+    let isGameOver = false
+
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    const switchPlayerTurn = () => {
+        if (activePlayer === playerOne) {
+            activePlayer = playerTwo
+        }
+        else {
+            activePlayer = playerOne
+        }
+    }
+
+    const checkWin = () => {
+        const board = Gameboard.getBoard()
+        for (let i = 0; i < winningCombinations.length; i++) {
+            const combo = winningCombinations[i]
+            if (board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]] && board[combo[0]] !== "") {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    function playRound(index) {
+        if (isGameOver === true) {
+            return
+        }
+        else {
+
+            const validMove = Gameboard.placeMark(activePlayer.mark, index)
+
+            if (!validMove) {
+                return
+            }
+
+            if (checkWin()) {
+                isGameOver = true
+                console.log(`${activePlayer.name} wins!`)
+                return
+            }
+            if (!Gameboard.getBoard().includes("")) {
+
+                isGameOver = true
+                console.log("It's tie")
+                return
+            }
+
+
+        }
+        switchPlayerTurn()
+    }
+
+    const getActivePlayer = () => {
+        return activePlayer
+    }
+
+    return {
+        playRound,
+        getActivePlayer
+    }
+
+
+})()
